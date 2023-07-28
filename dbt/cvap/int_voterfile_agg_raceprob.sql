da3e11c4254d94230d80c3cd4d0b7b772f74565c
irmacorado <65259720+irmacorado@users.noreply.github.com>
2023-07-28 09:31:10 -0400

    Update and rename int_voterfile_cd_agg_raceprob.sql to int_voterfile_agg_raceprob.sql

--- dbt/cvap/int_voterfile_agg_raceprob.sql
+++ dbt/cvap/int_voterfile_agg_raceprob.sql
@@ -1,8 +1,8 @@
-{{ config(materialized='table', sort=['statecode','congressionaldistrict'], dist = 'even') }}
+{{ config(materialized='table', sort=["statecode","countyname","congressionaldistrict","statehousedistrict","statesenatedistrict"], dist = 'even') }}
 
 with base as (
     select *
-    from {{ ref('int_voterfile_binned') }}
+    from {{ ref('int_votefile_binned') }}
     where 
         deceased is FALSE
         and voterstatus in ('active','inactive')
@@ -13,32 +13,40 @@ with base as (
         select 
         --geos
         statecode 
+        , countyname 
+        , countyfips
         , congressionaldistrict
+        , statehousedistrict
+        , statesenatedistrict
+        , uniqueprecinctcode
+        , precinctname
 
         --demos
         , {% if race =='nativeamerican' %}'Native American'{% elif race =='aapi' %}'{{race|upper}}'{% elif race =='other' %}'Unknown / Other'{% else %}'{{race|capitalize}}'{% endif %} as bin_race 
         /*we only want BIPOC aggregated since White and Other will already be available with bin_race*/
-        , {% if race in ('black','latinx','aapi','nativeamerican') %}'BIPOC'{% else %} null {% endif %} as bin_race_bipoc
+        , {% if race in ('black','latinx','aapi','nativeamerican') %}'BIPOC'{% elif race == 'other' %}'Unknown / Other'{% elif race == 'white' %}'White'{% endif %} as bin_race_bipoc
         , bin_age 
         , bin_gender 
         , bin_partyreg
 
         --models
+        , partisanshippropensity
+        , votepropensity
         , bin_partisanshippropensity
-        , bin_votepropensity
+        , bin_votepropensity 
 
         --vote history
         , case when e2020gst = statecode and e2020gvm is not null then 'voted' else 'didnt vote' end as e2020g
         , case when e2022gst = statecode and e2022gvm is not null then 'voted' else 'didnt vote' end as e2022g
-        
+
         --metric
         {% set column = 'prob_race_' + race|string %}
         /* here we use race probabilities instead of counting the individual people to help with the undercounting of BIPOC voters and the overcounting of White voters */
         , sum({{ column }}) as regvoters_tot
-        , sum(case when voterstatus = 'active' then {{ column }} end) as regvoters_tot_active
+        , sum(case when voterstatus = 'active' then {{ column }} end) as regvoters_tot_active 
         , sum(case when voterstatus = 'inactive' then {{ column }} end) as regvoters_tot_inactive
         from base
-        {{ dbt_utils.group_by(11)}}
+        {{ dbt_utils.group_by(19)}}
     )
 {% endfor %}
 
